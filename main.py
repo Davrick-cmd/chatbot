0f53e95a27000f32ef5582ed42405475b29ecf25
Herve Twahirwa <hervetwajean@gmail.com>
2024-10-24 19:54:02 +0200

    added a python script for predictions

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Ask a question or request data insights..."):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
