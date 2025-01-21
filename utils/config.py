f6005706d91f6087ecbb3dd0bea896a0ba768dea
Herve Twahirwa <hervetwajean@gmail.com>
2025-01-21 16:55:23 +0200

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -35,7 +35,7 @@ class Config:
     AD_TIMEOUT = 10  # seconds
     AD_SERVER = "bk.local"
     AD_DOMAIN = "bk.local"
-    AD_USER = 'datamanagementai'
-    AD_USER_PASSWORD = "Kigali2024@"
+    AD_USER = ''
+    AD_USER_PASSWORD = ""
     AD_TIMEOUT=10
     AD_SEARCH_BASE='DC=bk,DC=local'
